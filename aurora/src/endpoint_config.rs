8a8ee3ec2e25090de4a77d5592dc807877142c5e
hanbings <hanbings@hanbings.io>
2024-10-30 09:10:27 +0800

    👨‍🏭 Chore: Add Github actions.

--- aurora/src/endpoint_config.rs
+++ aurora/src/endpoint_config.rs
@@ -1,23 +1,41 @@
 use actix_web::{web::Data, Responder};
-use std::collections::HashMap;
+use iris_irides::raft::state::IrisRaftNodeState;
 use std::sync::Mutex;
 
 pub async fn get_config(
-    client: Data<Mutex<HashMap<String, String>>>,
+    node_state: Data<Mutex<IrisRaftNodeState>>,
 ) -> actix_web::Result<impl Responder> {
-    Ok(actix_web::web::Json(client.try_lock().unwrap().clone()))
+    let node_state = node_state.try_lock().unwrap();
+
+    Ok(actix_web::web::Json(node_state.clone().data))
 }
 
-pub async fn get_config_by_id() -> actix_web::Result<impl Responder> {
+pub async fn get_config_by_id(
+    node_state: Data<Mutex<IrisRaftNodeState>>,
+) -> actix_web::Result<impl Responder> {
+    let node_state = node_state.try_lock().unwrap();
+
     Ok(actix_web::web::Json({}))
 }
 
-pub async fn post_config() -> actix_web::Result<impl Responder> {
+pub async fn post_config(
+    node_state: Data<Mutex<IrisRaftNodeState>>,
+) -> actix_web::Result<impl Responder> {
+    let node_state = node_state.try_lock().unwrap();
+
     Ok(actix_web::web::Json({}))
 }
-pub async fn update_config() -> actix_web::Result<impl Responder> {
+pub async fn update_config(
+    node_state: Data<Mutex<IrisRaftNodeState>>,
+) -> actix_web::Result<impl Responder> {
+    let node_state = node_state.try_lock().unwrap();
+
     Ok(actix_web::web::Json({}))
 }
-pub async fn delete_config() -> actix_web::Result<impl Responder> {
+pub async fn delete_config(
+    node_state: Data<Mutex<IrisRaftNodeState>>,
+) -> actix_web::Result<impl Responder> {
+    let node_state = node_state.try_lock().unwrap();
+
     Ok(actix_web::web::Json({}))
 }
