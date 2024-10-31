ad7111e01bac6df67a1269588b77f3ca31df4644
hanbings <hanbings@hanbings.io>
2024-11-01 00:15:00 +0800

    ✨ Feat: Raft protocol for aurora config center.

--- aurora/src/endpoint_config.rs
+++ aurora/src/endpoint_config.rs
@@ -1,41 +1,101 @@
-use actix_web::{web::Data, Responder};
+use actix_web::{web, web::Data, Responder};
+use iris_irides::message::Message;
+use iris_irides::raft::client::IrisRaftClient;
 use iris_irides::raft::state::IrisRaftNodeState;
+use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
+
+#[derive(Serialize, Deserialize)]
+struct ConfigData {
+    key: String,
+    value: Option<String>,
+}
 
 pub async fn get_config(
     node_state: Data<Mutex<IrisRaftNodeState>>,
 ) -> actix_web::Result<impl Responder> {
-    let node_state = node_state.try_lock().unwrap();
+    let node_state = node_state.lock().unwrap();
 
-    Ok(actix_web::web::Json(node_state.clone().data))
+    Ok(web::Json(serde_json::json!(node_state.data.clone())))
+}
+
+#[derive(Serialize, Deserialize)]
+struct ConfigDataResponse {
+    key: String,
+    value: String,
 }
 
 pub async fn get_config_by_id(
     node_state: Data<Mutex<IrisRaftNodeState>>,
+    path: web::Path<String>,
 ) -> actix_web::Result<impl Responder> {
-    let _node_state = node_state.try_lock().unwrap();
+    let node_state = node_state.lock().unwrap();
+    let key = path.into_inner();
 
-    Ok(actix_web::web::Json({}))
+    if node_state.data.get(&key).is_none() {
+        return Ok(web::Json(serde_json::json!(Message::fail())));
+    }
+
+    let value = node_state.data.get(&key).unwrap();
+    let config = ConfigDataResponse {
+        key: key.clone(),
+        value: value.clone(),
+    };
+
+    Ok(web::Json(serde_json::json!(config)))
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    pub key: String,
+    pub value: Option<String>,
 }
 
 pub async fn post_config(
-    node_state: Data<Mutex<IrisRaftNodeState>>,
+    client: Data<Mutex<IrisRaftClient>>,
+    body: web::Json<Config>,
 ) -> actix_web::Result<impl Responder> {
-    let _node_state = node_state.try_lock().unwrap();
+    let client = client.lock().unwrap();
 
-    Ok(actix_web::web::Json({}))
+    if body.value.is_none() {
+        return Ok(web::Json({}));
+    }
+
+    client
+        .save(body.key.clone(), body.value.clone().unwrap())
+        .await;
+
+    Ok(web::Json({}))
 }
 pub async fn update_config(
-    node_state: Data<Mutex<IrisRaftNodeState>>,
+    client: Data<Mutex<IrisRaftClient>>,
+    path: web::Path<String>,
+    body: web::Json<Config>,
 ) -> actix_web::Result<impl Responder> {
-    let _node_state = node_state.try_lock().unwrap();
+    let client = client.lock().unwrap();
 
-    Ok(actix_web::web::Json({}))
+    if body.value.is_none() || body.key != path.into_inner() {
+        return Ok(web::Json(serde_json::json!(Message::fail())));
+    }
+
+    client
+        .update(body.key.clone(), body.value.clone().unwrap())
+        .await;
+
+    Ok(web::Json(serde_json::json!(Message::success())))
 }
 pub async fn delete_config(
-    node_state: Data<Mutex<IrisRaftNodeState>>,
+    client: Data<Mutex<IrisRaftClient>>,
+    path: web::Path<String>,
+    body: web::Json<Config>,
 ) -> actix_web::Result<impl Responder> {
-    let _node_state = node_state.try_lock().unwrap();
+    let client = client.lock().unwrap();
 
-    Ok(actix_web::web::Json({}))
+    if body.key != path.into_inner() {
+        return Ok(web::Json(serde_json::json!(Message::fail())));
+    }
+
+    client.delete(body.key.clone()).await;
+
+    Ok(web::Json(serde_json::json!(Message::success())))
 }
--- iris/README.md
+++ iris/README.md
@@ -4,6 +4,90 @@
 
 Iris 是一个针对分布式系统的工具集合
 
-- [ ] Raft 算法实现
+- [x] Raft 算法实现
 - [ ] Gossip 算法实现
 
+## 使用（呜呜没空写详细文档了）
+
+在 Rust 编写的分布式应用中使用 Iris ：
+
+（以 aurora 配置中心为例子）
+
+1. 在 Rust 应用中添加依赖：
+
+   ```toml
+   [package]
+   name = "aurora"
+   version = "0.1.0"
+   edition = "2021"
+   
+   [dependencies]
+   iris = { path = "../iris" }
+   log = "0.4"
+   env_logger = "0.11"
+   uuid = { version = "1.11.0", features = ["v4", "serde"] }
+   
+   actix-web = "4"
+   tokio = { version = "1.0", features = ["full"] }
+   serde = { version = "1", features = ["derive"] }
+   serde_json = "1"
+   reqwest = "0.12.8"
+   ```
+   
+2. 创建 State 在 Actix 中共享上下文：
+
+   ```rust
+   use iris_irides::raft::client::IrisRaftClient;
+   use iris_irides::raft::config::IrisRaftConfig;
+   use iris_irides::raft::state::{IrisRaftClock, IrisRaftNodeState};
+   
+   let clock = Data::new(Mutex::new(IrisRaftClock::new()));
+   let client = Data::new(Mutex::new(IrisRaftClient::new(config.endpoint.clone())));
+   let node_state = Data::new(Mutex::new(IrisRaftNodeState::new(
+       IrisRaftConfig::no_log_compaction(
+           config.id,
+           config.secret,
+           config.endpoint,
+           // heartbeat timeout
+           200,
+           // election timeout (range random)
+           (300, 800),
+       ),
+   )));
+   ```
+
+   > 注意：需要自己处理配置文件的读取
+   > 可以选择 figment 作为配置文件中间层
+   > `figment = { version = "0.10", features = ["toml"] }`
+
+3. 将 AppState 注入到 actix 上下文，并注册路由
+
+   ```rust
+   // 忽略了 actix use 引言，需自行补全
+   
+   HttpServer::new(move || {
+       App::new()
+           .app_data(Data::clone(&clock))
+           .app_data(Data::clone(&client))
+           .app_data(Data::clone(&node_state))
+           // metadata
+           .route("/cluster/node", web::get().to(iris_irides::raft::endpoint_metadata::get_node))
+           .route("/cluster/nodes", web::get().to(iris_irides::raft::endpoint_metadata::get_nodes))
+           .route("/cluster/status",web::get().to(iris_irides::raft::endpoint_metadata::get_status))
+           // check
+           .route("/cluster/check",web::post().to(iris_irides::raft::endpoint_check::post_check))
+           // action
+           .route("/cluster/append",web::post().to(iris_irides::raft::endpoint_action::post_append))
+           .route("/cluster/commit",web::post().to(iris_irides::raft::endpoint_action::post_commit))
+           .route("/cluster/vote",web::post().to(iris_irides::raft::endpoint_action::post_vote))
+   })
+   .bind((config.ip, config.port))?
+   .run()
+   .await
+   ```
+   
+   > endpoint 的形式与注册的 actix 的路由有关，例如本例子中注册到 `/cluster` 下，那么 endpoint 为 `http://127.0.0.1/cluster`
+   >
+   > 您也可以选用其他路由端点，但暂不支持为每一个接口都设置路由前缀
+   >
+   > endpoint 需要携带协议名称 http 或 https
